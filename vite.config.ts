a3f8ece6bbd2502dc60ad82673f4da0d6a2b8527
Jack Hygate <jack.hygate@myurbanjungle.com>
2025-12-28 19:26:49 +0000

    Add deployment to jhygate.github.io/looppad

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
+  base: '/looppad/', // Base path for deployment to jhygate.github.io/looppad/
   root: 'src', // Serve from src directory
   publicDir: resolve(__dirname, 'public'), // Serve static assets from public
   build: {
